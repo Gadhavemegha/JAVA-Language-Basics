9c21a4869fe949c9929e74c77d326be024b51ca9
Gadhavemegha <108414205+Gadhavemegha@users.noreply.github.com>
2022-06-30 11:58:31 +0530

    Create prg3.java

---
+++ Exercise1/prg3.java
@@ -0,0 +1,28 @@
+//3.Write a program for a Single line comment, multi-line and documentation comments 
+
+package exercise1;
+public class comment {
+
+	public static void main(String[] args) {
+		//This is single line comment  
+		
+		
+		/*  
+		This   
+		is   
+		multi line   
+		comment  
+		*/ 
+		
+		
+		/**  
+		* 
+		*We can use various tags to depict the parameter 
+		*or heading or author name 
+		*We can also use HTML tags   
+		* 
+		*/  
+
+	}
+
+}
