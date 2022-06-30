eb09db0fe555a78c119986d0e861d396ca52547d
Gadhavemegha <108414205+Gadhavemegha@users.noreply.github.com>
2022-06-30 11:59:47 +0530

    Create prg6.java

---
+++ Exercise1/prg6.java
@@ -0,0 +1,19 @@
+
+//6.Write a function to print your name and call the function from main method 
+
+package exercise1;
+public class callname 
+{
+	public void name()
+	{
+		System.out.println("My name is Megha Ashok Gadhave");
+	}
+
+	public static void main(String[] args) 
+	{
+		callname n1 = new callname();
+		n1.name();
+	}
+
+}
+
