bf959fe1be4fb1a9abb26ab63d75bd0d555fa246
Gadhavemegha <108414205+Gadhavemegha@users.noreply.github.com>
2022-06-30 12:07:40 +0530

    Create prg9.java

---
+++ Exercise3/prg9.java
@@ -0,0 +1,28 @@
+//9. Write a program to find the prime or not. 
+package exercise3;
+
+public class prg9 
+{
+	 public static void main(String[] args) 
+	 {
+
+		    int num = 29;
+		    boolean flag = false;
+		    for (int i = 2; i <= num / 2; ++i) 
+		    {
+		      // condition for nonprime number
+		    	if (num % i == 0) 
+		    	{
+		    		flag = true;
+		    		break;
+		    	}
+		    }
+
+		    if (!flag)
+		      System.out.println(num + " is a prime number.");
+		    else
+		      System.out.println(num + " is not a prime number.");
+	 }
+}
+
+
