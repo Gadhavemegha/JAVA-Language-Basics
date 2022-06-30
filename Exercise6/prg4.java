a9f22fe86802fc359bf805ad6c7f8edc7c75f1d8
Gadhavemegha <108414205+Gadhavemegha@users.noreply.github.com>
2022-06-30 13:59:56 +0530

    Create prg4.java

---
+++ Exercise6/prg4.java
@@ -0,0 +1,14 @@
+//4. Extract a string using Substring 
+
+package exercise6;
+
+public class prg4 
+{
+	public static void main(String args[])
+	{
+		String s1="jalaacademy";  
+		System.out.println(s1.substring(2,4));//returns
+		System.out.println(s1.substring(1,3));
+		System.out.println(s1.substring(2));
+	}
+}
