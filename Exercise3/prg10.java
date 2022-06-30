ba0a8517da741ffb57cd9af00b389376d877fb69
Gadhavemegha <108414205+Gadhavemegha@users.noreply.github.com>
2022-06-30 12:08:07 +0530

    Create prg10.java

---
+++ Exercise3/prg10.java
@@ -0,0 +1,29 @@
+//10. Write a program to palindrome or not.  
+package exercise3;
+
+import java.util.Scanner;
+class expalindrome 
+{
+		public static void main(String args[]) 
+		{
+				int x,number, y=0,temp=0;
+				Scanner s=new Scanner(System.in);
+				System.out.println("Enter any number: ");
+				number=s.nextInt();
+				x=number;
+				while(number>0)
+				{
+						x=number%10;
+						number=number/10;
+						temp=temp*10+x;
+				}
+				if(temp==y)
+				{
+						System.out.println("Number is Palindrome");
+				}
+				else
+				{
+						System.out.println("not Palindrome");
+				}
+		}
+}
