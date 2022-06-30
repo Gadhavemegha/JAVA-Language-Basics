d5b845b319567d2baa9d68d2d0b6c382578e870a
Gadhavemegha <108414205+Gadhavemegha@users.noreply.github.com>
2022-06-30 12:02:15 +0530

    Create prg3.java

---
+++ Exercise2/prg3.java
@@ -0,0 +1,20 @@
+//Write a program to find the two numbers equal or not. 
+
+package exercise2;
+
+public class equalno {
+
+	public static void main(String[] args) {
+
+		int no1 = 5, no2 = 10;
+		if (no1 == no2)
+		{
+				System.out.println(no1 + " and " + no2 + " are Equal");
+		}
+		else
+		{
+				System.out.println(no1 + " and " + no2 + " are Not Equal");
+		}
+	}
+
+}
