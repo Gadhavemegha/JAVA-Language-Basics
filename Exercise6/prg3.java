e96e124d7c9eacd517f4166039903a8c0f72eab1
Gadhavemegha <108414205+Gadhavemegha@users.noreply.github.com>
2022-06-30 13:59:39 +0530

    Create prg3.java

---
+++ Exercise6/prg3.java
@@ -0,0 +1,13 @@
+//3. Finding the length of the string 
+
+package exercise6;
+
+public class prg3 
+{
+	public static void main(String args[])
+	{
+		String name = "educative";  //Initilizing a String Object name
+	    int length = name.length();  //Calling the inbuilt lenght() method
+	    System.out.println("The length of the String \""+name+"\" is: " +length);    
+	}
+}
