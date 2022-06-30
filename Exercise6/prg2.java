d90b8a9bfe00c0a12a1c56695589c2e3947158af
Gadhavemegha <108414205+Gadhavemegha@users.noreply.github.com>
2022-06-30 13:59:21 +0530

    Create prg2.java

---
+++ Exercise6/prg2.java
@@ -0,0 +1,14 @@
+//2.Concatenating two strings using + operator 
+
+package exercise6;
+
+public class prg2 
+{
+	public static void main(String args[])
+	{
+		String s1 = "Megha";
+		String s2 = "Gadhave";
+		String str = s1 + s2;
+		System.out.println("Concatenated String is:" + str);
+	}
+}
