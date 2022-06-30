f1a8658589283da1dae370301b0503b84875d6e9
Gadhavemegha <108414205+Gadhavemegha@users.noreply.github.com>
2022-06-30 12:01:58 +0530

    Create prg2.java

---
+++ Exercise2/prg2.java
@@ -0,0 +1,16 @@
+// Program to equal operator and not equal operators 
+
+package exercise2;
+
+public class eqoperator 
+{
+
+	public static void main(String[] args) 
+	{
+		int a = 5, b = 6;
+		
+		System.out.println(a == b); //false
+		System.out.println(a != b); //true
+	}
+
+}
