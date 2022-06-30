83d6013ca6cebafc9a04bb649dda96a2117da62a
Gadhavemegha <108414205+Gadhavemegha@users.noreply.github.com>
2022-06-30 11:58:58 +0530

    Create prg4.java

---
+++ Exercise1/prg4.java
@@ -0,0 +1,22 @@
+
+//4.Define variables for different Data Types int, Boolean, char, float, double and print on the console. 
+package exercise1;
+public class variablesinjava {
+
+	public static void main(String[] args) 
+	{
+		int a = 2;
+		boolean b1 = true;
+		char c1 = 'h';
+		float f1 = 2.5467f ;
+		double d1 = 4.5674d;
+		System.out.println(a);
+		System.out.println(b1);
+		System.out.println(c1);
+		System.out.println(f1);
+		System.out.println(d1);
+		
+	}
+
+}
+
