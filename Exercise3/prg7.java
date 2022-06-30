f387a0733ea94957a53d21d05f0eb2c9cf2fc67c
Gadhavemegha <108414205+Gadhavemegha@users.noreply.github.com>
2022-06-30 12:06:58 +0530

    Create prg7.java

---
+++ Exercise3/prg7.java
@@ -0,0 +1,16 @@
+//7. Write a program to print 1 to 10 using the do-while loop statement. 
+package exercise3;
+
+public class prg7 
+{
+	public static void main(String args[])
+	{
+		int i = 1;
+		
+		do
+		{
+			System.out.println(i);
+			i++;
+		 } while (i<=10);
+	}
+}
