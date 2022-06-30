8ec7e277466eac51ac6f5061eff708ec60187711
Gadhavemegha <108414205+Gadhavemegha@users.noreply.github.com>
2022-06-30 12:05:15 +0530

    Create prg2.java

---
+++ Exercise3/prg2.java
@@ -0,0 +1,17 @@
+//Write a java program to print 1 to 20 numbers using the while loop
+package exercise3;
+
+public class prg2 
+{
+
+	public static void main(String[] args) 
+	{
+		int i = 1;
+		while(i<=20)
+		{
+			System.out.println(i);
+			i++;
+		}
+	}
+
+}
