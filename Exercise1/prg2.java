ab7652635ef447f47ea94ec87e73545400e9b312
Gadhavemegha <108414205+Gadhavemegha@users.noreply.github.com>
2022-06-30 11:57:58 +0530

    Create prg2.java

---
+++ Exercise1/prg2.java
@@ -0,0 +1,13 @@
+
+//2. Write a program to print your name. 
+
+package exercise1;
+public class printname {
+
+	public static void main(String[] args)
+	{
+		System.out.println("Hello My name is MEGHA...");
+
+	}
+
+}
