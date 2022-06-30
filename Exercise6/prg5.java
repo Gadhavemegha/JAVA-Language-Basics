8cb57ec029d7759a86170e54624abbd13664aea6
Gadhavemegha <108414205+Gadhavemegha@users.noreply.github.com>
2022-06-30 14:00:14 +0530

    Create prg5.java

---
+++ Exercise6/prg5.java
@@ -0,0 +1,11 @@
+//5. Searching in strings using indexOf() 
+package exercise6;
+
+public class prg5 
+{
+	public static void main(String args[])
+	{
+		String myStr = "Hello planet earth, you are a great planet.";
+	    System.out.println(myStr.indexOf("e", 5)); //first occurrence of the letter "e" in a string, starting the search at position 5:
+	}
+}
