1468141b7403e109abe6e70b8e30b685dd8baf87
Gadhavemegha <108414205+Gadhavemegha@users.noreply.github.com>
2022-06-30 14:03:44 +0530

    Create prg14.java

---
+++ Exercise6/prg14.java
@@ -0,0 +1,13 @@
+//14.  Converting to uppercase and lowercase 
+
+package exercise6;
+
+public class prg14 
+{
+	public static void main(String args[])
+	{
+		String txt = "Hello World";
+		System.out.println(txt.toUpperCase());
+		System.out.println(txt.toLowerCase());
+	}
+}
